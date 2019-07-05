d6210678b04cd6e6ef923198dfda78e9703068b9
jay <nielcansino@gmail.com>
2019-07-05 23:09:57 +0900

    created test_int_heap.cpp; tested Heap for both min and max mode for <int> datatype

--- heap/tests/test_int_heap.cpp
+++ heap/tests/test_int_heap.cpp
@@ -1,7 +1,36 @@
 #include "gtest/gtest.h"
 #include "heap.hpp"
 
+/*Test module for Heap<int>*/
 namespace {
+  class MinHeapFixture : public ::testing::Test {
+   protected:
+
+    MinHeapFixture() {
+    // You can do set-up work for each test here.
+    }
+
+    ~MinHeapFixture() override {
+    // You can do clean-up work that doesn't throw exceptions here.
+    }
+
+    void SetUp() override {
+      my_min_heap->insert(1);
+      my_min_heap->insert(2);
+      my_min_heap->insert(2);
+      my_min_heap->insert(3);
+      my_min_heap->insert(4);
+      my_min_heap->insert(5);
+      my_min_heap->insert(6);
+      my_min_heap->insert(8);
+      my_min_heap->insert(9);
+    }
+    void TearDown() override {
+      delete my_min_heap;
+    }
+
+    Heap<int> *my_min_heap = new Heap<int>(0);
+  };
   class MaxHeapFixture : public ::testing::Test {
    protected:
 
@@ -24,6 +53,7 @@ namespace {
       my_max_heap->insert(8);
       my_max_heap->insert(9);
     }
+
     void TearDown() override {
       delete my_max_heap;
     }
@@ -31,15 +61,18 @@ namespace {
     Heap<int> *my_max_heap = new Heap<int>(1);
   };
 
-  TEST(MaxHeap, BlankHeap) {
+  TEST(IntHeap, BlankHeap) {
     Heap<int> *my_max_heap = new Heap<int>(1);
+    Heap<int> *my_min_heap = new Heap<int>(0);
     //check size
     EXPECT_EQ(0, my_max_heap->size());
+    EXPECT_EQ(0, my_min_heap->size());
     //extract max
     EXPECT_EQ(-1, my_max_heap->extract_extreme());
+    EXPECT_EQ(-1, my_min_heap->extract_extreme());
   }
 
-  TEST(MaxHeap, CreateHeap) {
+  TEST(IntHeap, CreateHeap) {
     Heap<int> *my_max_heap = new Heap<int>(1);
     //check size
     EXPECT_EQ(0, my_max_heap->size());
@@ -103,6 +136,56 @@ namespace {
     EXPECT_EQ(1, my_max_heap->extract_extreme());
   }
 
+  TEST_F(MinHeapFixture, ExtractMin) {
+    /*Make sure that this fixture has the same name as the setup class declared above*/
+    EXPECT_EQ(9, my_min_heap->size()); //check size
+    //Extract min #1
+    //my_min_heap->print_heap();
+    EXPECT_EQ(1, my_min_heap->extract_extreme());
+    EXPECT_EQ(2, my_min_heap->extract_extreme());
+    EXPECT_EQ(2, my_min_heap->extract_extreme());
+    EXPECT_EQ(3, my_min_heap->extract_extreme());
+    EXPECT_EQ(4, my_min_heap->extract_extreme());
+    EXPECT_EQ(5, my_min_heap->extract_extreme());
+    EXPECT_EQ(6, my_min_heap->extract_extreme());
+    EXPECT_EQ(8, my_min_heap->extract_extreme());
+    EXPECT_EQ(9, my_min_heap->extract_extreme());
+  }
+
+  TEST_F(MinHeapFixture, ViewMin) {
+    /*Make sure that this fixture has the same name as the setup class declared above*/
+    EXPECT_EQ(9, my_min_heap->size()); //check size
+    //View min repeatedly
+    //my_min_heap->print_heap();
+    EXPECT_EQ(1, my_min_heap->view_extreme());
+    EXPECT_EQ(1, my_min_heap->view_extreme());
+    EXPECT_EQ(1, my_min_heap->view_extreme());
+  }
+
+  TEST_F(MinHeapFixture, Insert) {
+    /*Make sure that this fixture has the same name as the setup class declared above*/
+    std::cout << "Heap before inserting:" << std::endl;
+    //my_min_heap->print_heap();
+    //Insert #1
+    my_min_heap->insert(4);
+    std::cout << "Heap after inserting 4:" << std::endl;
+    //my_min_heap->print_heap();
+    my_min_heap->insert(13);
+    std::cout << "Heap after inserting 13:" << std::endl;
+    //my_min_heap->print_heap();
+    //Extract min#1
+    EXPECT_EQ(1, my_min_heap->extract_extreme());
+    EXPECT_EQ(2, my_min_heap->extract_extreme());
+    EXPECT_EQ(2, my_min_heap->extract_extreme());
+    EXPECT_EQ(3, my_min_heap->extract_extreme());
+    EXPECT_EQ(4, my_min_heap->extract_extreme());
+    EXPECT_EQ(4, my_min_heap->extract_extreme());
+    EXPECT_EQ(5, my_min_heap->extract_extreme());
+    EXPECT_EQ(6, my_min_heap->extract_extreme());
+    EXPECT_EQ(8, my_min_heap->extract_extreme());
+    EXPECT_EQ(9, my_min_heap->extract_extreme());
+    EXPECT_EQ(13, my_min_heap->extract_extreme());
+  }
 }  // namespace
 
 int main(int argc, char **argv) {
